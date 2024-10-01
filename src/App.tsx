4fdb06cd692c1fc1be02f2f06807c65f9875b8bb
BlokOne <90694470+BlokOne@users.noreply.github.com>
2024-10-01 12:57:18 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,6 @@ import "./App.css";
 import { Home } from './pages'
 
 function App() {
-  return (
-    <Home title="Hello World" />
-    // <Grid2  >
-    //   <Button sx={{
-    //     bgcolor: (theme: Theme) => theme.palette.text.primary,
-    //     color: (theme: Theme) => theme.palette.background.default
-    //   }} onClick={handleClick}>Open Snackbar</Button>
-    //   
-    // </Grid2>
-  );
+  return (<Home title="Hello World" />);
 }
 export default App;
