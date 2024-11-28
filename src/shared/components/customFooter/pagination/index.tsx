092364e756e3b6201f5b143d174e9434328f19f3
EvgeN19840 <107992155+EvgeN19840@users.noreply.github.com>
2024-11-28 15:48:21 +0100

    fixed pagination

--- src/shared/components/customFooter/pagination/index.tsx
+++ src/shared/components/customFooter/pagination/index.tsx
@@ -12,9 +12,15 @@ export const CustomPagination = () => {
   const pageCount = useGridSelector(apiRef, gridPageCountSelector);
   const totalRows = apiRef.current.getRowsCount();
 
+  const startRow = paginationModel.page * paginationModel.pageSize + 1;
+  const endRow = Math.min(
+    (paginationModel.page + 1) * paginationModel.pageSize,
+    totalRows
+  );
+
   return (
     <Box display="flex" alignItems="center" justifyContent="flex-end" gap={2}>
-      <Typography variant="body2">{`Showing ${paginationModel.pageSize} of ${totalRows} rows`}</Typography>
+      <Typography variant="body2">{`Showing rows ${startRow}-${endRow} of ${totalRows}`}</Typography>
       <Pagination
         count={pageCount}
         page={paginationModel.page + 1}
