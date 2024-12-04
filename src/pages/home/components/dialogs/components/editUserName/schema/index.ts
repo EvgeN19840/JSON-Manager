f1c043fb998dd2f5bb2d35746ba9b8e842c54fd7
EvgeN19840 <107992155+EvgeN19840@users.noreply.github.com>
2024-12-04 11:08:21 +0100

    deleted validation edit employees

--- src/pages/home/components/dialogs/components/editUserName/schema/index.ts
+++ src/pages/home/components/dialogs/components/editUserName/schema/index.ts
@@ -2,15 +2,11 @@
 import * as yup from 'yup'
 
 export const schema = yup.object().shape({
-  firstName: yup.string().required('First Name is required').matches(/^[A-Za-z0-9\s]+$/, 'First Name must only contain Latin letters, numbers, and symbols"'),
-  lastName: yup.string().required('Last Name is required').matches(/^[A-Za-z0-9\s]+$/, 'Last Name must only contain Latin letters, numbers, and symbols"'),
+  firstName: yup.string().required('First Name is required'),
+  lastName: yup.string().required('Last Name is required'),
   birthDate: yup
     .string()
-    .required('Birth date is required')
-    .matches(
-      /^\d{4}-\d{2}-\d{2}$/,
-      'Birth date must be in the format YYYY-MM-DD'
-    ),
+  ,
   eId: yup
     .number()
     .required()
