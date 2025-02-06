51c2d98112fc96937f6086a68377b91f59503f04
EvgeN19840 <107992155+EvgeN19840@users.noreply.github.com>
2025-02-06 11:42:31 +0100

    added button cancel

--- src/pages/home/components/dialogs/components/duplicateEmployee/index.tsx
+++ src/pages/home/components/dialogs/components/duplicateEmployee/index.tsx
@@ -1,12 +1,13 @@
 // ** MUI
-import { FormWrapper } from "@/shared/formElements";
+import { FormFooter, FormWrapper } from "@/shared/formElements";
 import { InputField } from "@/shared/inputField";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 // ** Hooks
 import { useModal } from "@/hooks/useModal";
 import { useDataStateContext } from "@/hooks/useDataStateContext";
 import { useHandleAddItem } from "@/hooks/useAddItem";
+import { useForm } from "react-hook-form";
 
 export const DuplicateEmployee = () => {
   const { closeDialog } = useModal() as { closeDialog: () => void };
@@ -14,47 +15,53 @@ export const DuplicateEmployee = () => {
     useDataStateContext();
   const handleAddItem = useHandleAddItem();
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCountDuplicates(event.target.value);
-  };
+  const { handleSubmit } = useForm();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const onSubmit = () => {
     handleAddItem({
       type: "employees",
       item: data.employees[0],
       eId: eIdSelectedEmployee!,
     });
     closeDialog();
-    setCountDuplicates('1')
+    setCountDuplicates("1");
   };
 
   return (
     <Box>
-      <FormWrapper title="Duplicate" onSubmit={handleSubmit}>
+      <FormWrapper title="Duplicate" onSubmit={handleSubmit(onSubmit)}>
         <Box
           sx={{
             display: "flex",
-            justifyContent: "center",
+            justifyContent: "space-between",
             alignItems: "center",
             gap: 2,
+            mx: 2,
           }}
         >
           <Typography variant="h6">Enter the number of duplicates:</Typography>
           <InputField
+            label="Number of duplicates"
+            placeholder="Enter count"
             value={countDuplicates}
-            onChange={handleInputChange}
-            placeholder="Enter number"
-            sx={{ width: "100px", m: 2 }}
+            onChange={(event) => setCountDuplicates(event.target.value)}
+            sx={{ width: "150px" }}
           />
         </Box>
-
-        <Box sx={{ display: "flex", justifyContent: "center", pt: 1 }}>
-          <Button type="submit" variant="contained">
-            Duplicate
-          </Button>
-        </Box>
       </FormWrapper>
+      <Box
+        sx={{
+          mx: 2,
+        }}
+      >
+        <FormFooter
+          cancelButtonText="Cancel"
+          actionButtonText="Duplicate"
+          showSecondButton={true}
+          buttonAction={handleSubmit(onSubmit)}
+          source="general"
+        />
+      </Box>
     </Box>
   );
 };
