2e4b72bb82cacbb8d654b3461d856ac6abb4a82c
Andrei Konovalov <andrew.konovalov@vizconsulting.com>
2025-03-18 15:18:43 +0000

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+SRC=./backend/dist
+DST=/var/www/test-metrics
+
+echo "Starting deploy"
+echo "Removing binaries in $DST"
+rm -rf $DST/*
+
+echo "Copying binaries from $SRC"
+cp -R $SRC/* $DST/
+
+chown -R www-data:www-data $DST/*
+
+rm -rf $SRC/
+
+echo "Finished"
