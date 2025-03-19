2deb6846b3f0c704cc38c18fcfe9568b4b89ded3
EvgeN19840 <107992155+EvgeN19840@users.noreply.github.com>
2025-03-19 19:49:47 +0100

    delete vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,11 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: "./",
+  base: "./", 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   plugins: [react()],
 });
-
