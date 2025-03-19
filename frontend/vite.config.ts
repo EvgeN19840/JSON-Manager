afc7dd8c76e935c22a53eb486f71cab17995bfcc
EvgeN19840 <107992155+EvgeN19840@users.noreply.github.com>
2025-03-19 19:25:38 +0100

    1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,15 +3,12 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
+  base: "./",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   plugins: [react()],
-  base: "./", 
-  build: {
-    outDir: "dist", 
-    emptyOutDir: true, 
-  },
 });
+
