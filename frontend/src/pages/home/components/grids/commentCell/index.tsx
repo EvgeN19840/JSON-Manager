4281185947ec9822f52f062c22e599fee1d2361f
EvgeN19840 <107992155+EvgeN19840@users.noreply.github.com>
2025-03-21 15:15:02 +0100

    autosave comment

--- frontend/src/pages/home/components/grids/commentCell/index.tsx
+++ frontend/src/pages/home/components/grids/commentCell/index.tsx
@@ -4,6 +4,7 @@ import { GridRenderCellParams } from '@mui/x-data-grid'
 import { IEmployee } from '@/types/json'
 import { useDataStateContext } from '@/pages/home/hooks/useDataStateContext'
 import { Box } from '@mui/material'
+import { saveEmployeeToLocalStorage } from '@/services/storageService'
 
 interface Props {
   params: GridRenderCellParams<IEmployee>
@@ -17,20 +18,18 @@ export const CommentCell: React.FC<Props> = ({ params }) => {
     const newComment = e.target.value
     setLocalValue(newComment)
 
+    const updatedEmployee: IEmployee = { ...params.row, comment: newComment }
     setData(prev => {
       if (!prev?.employees) return prev
 
-      const updatedEmployees = prev.employees.map(emp =>
-        emp.eId === params.row.eId ? { ...emp, comment: newComment } : emp
-      )
-
+      const updatedEmployees = prev.employees.map(emp => (emp.eId === updatedEmployee.eId ? updatedEmployee : emp))
       return {
         ...prev,
         employees: updatedEmployees
       }
     })
+    saveEmployeeToLocalStorage(updatedEmployee)
   }
-
   return (
     <Box onKeyDown={e => e.stopPropagation()} width='100%'>
       <InputField value={localValue} onChange={handleChange} placeholder='Type your comment...' noBorderRadius />
