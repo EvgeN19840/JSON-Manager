7104e325213c7b2e538cb5b758fbaa7e221734f7
EvgeN19840 <107992155+EvgeN19840@users.noreply.github.com>
2025-03-24 17:10:10 +0100

    delete console.log

--- frontend/src/shared/utils/listTemplate/index.ts
+++ frontend/src/shared/utils/listTemplate/index.ts
@@ -4,7 +4,6 @@ import { ITypeJSON, IEmployee } from "@/types/json";
 
 export const listTemplate = (): ITypeJSON => {
     const localStorageEmployees: IEmployee[] = getAllLocalStorage();
-    console.log(" localStorageEmployees", localStorageEmployees);
     const mergedEmployees: IEmployee[] = [...localStorageEmployees, ...employeeData.employees];
 
     return {
