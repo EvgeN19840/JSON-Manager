07a76a64594d5d2a4b24d41be0b9db6a3cf81988
BlokOne <90694470+BlokOne@users.noreply.github.com>
2025-03-25 14:23:37 +0300

    fix config

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -3,9 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const dbConfig = {
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 5432,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT ? parseInt(process.env.PGPORT, 10) : 5432,
 };
