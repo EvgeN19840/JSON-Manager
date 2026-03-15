cc1faa9a67a234a563393e1019c4b410795e6b24
EvgeN19840 <107992155+EvgeN19840@users.noreply.github.com>
2026-03-15 08:07:57 +0100

    test buttons count

--- frontend/e2e/tests/classTest/index.spec.ts
+++ frontend/e2e/tests/classTest/index.spec.ts
@@ -1,4 +1,4 @@
-import {  expect, test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 test("Test class", async ({ page }) => {
 
@@ -6,7 +6,9 @@ test("Test class", async ({ page }) => {
     const pageProject = await page.context().newPage()
     await pageProject.goto('https://ci-cd-json-manager.netlify.app')
 
-     await expect(page).toHaveURL(/netlify/);
+    //  await expect(page).toHaveURL(/netlify/);
+    const allButtons = await page.getByRole('button').count()
+    expect(allButtons).toEqual(5)
 
 
 })
